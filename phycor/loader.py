aad5d77ff3fae66aeb8db8b231f4608b582b9e5d
Ratan Sebastian <ratanjagan@gmail.com>
2022-03-18 17:07:36 +0100

    add verbosity

--- phycor/loader.py
+++ phycor/loader.py
@@ -1,6 +1,8 @@
 from poppler import load_from_file, PageRenderer
 from glob import glob
 import numpy as np
+from tqdm import tqdm
+
 
 class Loader:
     def __init__(self, data_dir):
@@ -10,16 +12,19 @@ class Loader:
     def list_files(self):
         return glob(self.data_dir + '/books/*.pdf')
 
-    def files(self):
-        for filename in self.list_files():
+    def files(self, verbose):
+        for filename in tqdm(self.list_files(), desc='overall') if verbose else self.list_files():
             yield filename, load_from_file(filename)
 
-    def page_images(self):
-        for filename, file in self.files():
-            for page_index in range(file.pages):
+    def page_images(self, verbose):
+        for filename, file in self.files(verbose):
+            for page_index in self.pages_progress(filename, file.pages, verbose):
                 poppler_image = self.renderer.render_page(file.create_page(page_index))
                 image = poppler_to_rgb(poppler_image)
                 yield filename.split('/')[-1], page_index, image
+
+    def pages_progress(self, filename, page_count, verbose):
+        return tqdm(range(page_count), total=page_count, desc=filename) if verbose else range(page_count)
 
 
 def poppler_to_rgb(image):
--- phycor/main.py
+++ phycor/main.py
@@ -20,25 +20,27 @@ logging.basicConfig(
 def parse_args():
     parser = argparse.ArgumentParser(description="Parse Physics Textbooks")
     #  --data_dir ../data/ --model_type fast --output_path ../data/fast_parse/ --n_pages 10
-    parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose output", default=False)
     parser.add_argument("-i", "--data_dir", help="Data Dir")
     parser.add_argument("-o", "--output_path", help="Output Dir")
     parser.add_argument("-m", "--model_type", help="Model Type", choices=['fast', 'efficient'])
-    parser.add_argument("-n", "--n_pages", help="Num. Pages", type=int)
+    parser.add_argument("-n", "--n_pages", help="Num. Pages", type=int, required=False, default=None)
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
-    run(args.data_dir, args.output_path, args.model_type, args.n_pages)
+    run(args.data_dir, args.output_path, args.model_type, args.n_pages, args.verbose)
 
 
-def run(data_dir, output_path, model_type, n_pages):
+def run(data_dir, output_path, model_type, n_pages, verbose):
     loader = Loader(data_dir)
     parser = Parser(model_type)
     writer = Writer(output_path)
-    for filename, page_index, image in islice(loader.page_images(), n_pages):
+    page_images = loader.page_images(verbose)
+    optionally_limited = islice(page_images, n_pages) if n_pages else page_images
+    for filename, page_index, image in optionally_limited:
         elements = parser.get_text_areas(image)
         for el_idx, (crop, el_type, score) in enumerate(elements):
             writer.write(filename, page_index, el_idx, el_type, score, Image.fromarray(image), crop)
